16c54cf10c18c39085bf2870c183db4afb08e15e
Andres Plana <dudeandu@hotmail.com>
2022-11-30 22:23:45 -0500

    added a wireframe image, and wriote the pseudo code

--- src/App.js
+++ src/App.js
@@ -11,3 +11,13 @@ function App() {
 }
 
 export default App;
+
+// Create an array that will contain the information as ones and zeros that will be used to fill the squares. Could be a 3D array if that helps locate square components in the drawing board easier.
+
+// As the page loads use useEffect to generate a grid of squares (aka drawing board) using a map function to append(right word?!) components to a container in App.js. Each component will have an id that will be used to track changes 
+
+// Once al components are loaded a call will be made to firebase to get the current status of the array, and the use a map function in combination with useState to draw the squares to the current color
+
+// Each square component in the drawing board will have an onClick function that will pass some props [index: for locating within the array and color: to generate the colour] to an onChange function which will then use the set function from firebse to update the array on the database
+
+// I feel like I'm missing something... 
